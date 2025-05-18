ab7b3b941d581caa82d607ce4b424611245e0af5
FranSanchez <franciscosanchezp44@gmail.com>
2025-05-17 21:20:49 -0300

    cards terminadas

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// const products = [
+//     // tortas productos
+//     {id: 1, name: 'Matilda', price: 10000, image: 'image/#'}
+//     {id: 2, name: 'Red Velvet', price: 9000, image: 'image/#'}
+//     {id: 3, name: 'Marquisse', price: 8000, image: 'image/#'}
+//     {id: 4, name: 'Rogel', price: 8900, image: 'imgage/#'}
+//     {id 5, name: 'Chocotorta', price: 11500, image: 'image/#'}
+
+//     // batidos
+    
+// ]
