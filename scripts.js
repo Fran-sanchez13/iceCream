9623c8220c410da733acca3a94ede6d203984009
FranSanchez <franciscosanchezp44@gmail.com>
2025-05-30 20:13:05 -0300

    media query

--- scripts.js
+++ scripts.js
@@ -4,8 +4,17 @@
 //     {id: 2, name: 'Red Velvet', price: 9000, image: 'image/#'}
 //     {id: 3, name: 'Marquisse', price: 8000, image: 'image/#'}
 //     {id: 4, name: 'Rogel', price: 8900, image: 'imgage/#'}
-//     {id 5, name: 'Chocotorta', price: 11500, image: 'image/#'}
+//     {id: 5; 'Chocotorta', price: 11500, image: 'image/#'}
 
 //     // batidos
+//     {id: 6, name: 'Frutilla', price: 1000, image: 'image/#'}
+//     {id: 7, name: 'Kiwi', price: 9000, image: 'image/#'}
+//     {id: 8, name: 'Durazno', price: 8000, image: 'image/#'}
+    
+//     //helados
+//     {id: 9, name: 'Vainilla a la crema', price: 8900, image: 'imgage/#'}
+//     {id 10, name: 'Chocolate', price: 11500, image: 'image/#'}
+//     {id 10, name: 'Cafe', price: 11500, image: 'image/#'}
+//     {id 10, name: 'Coco', price: 11500, image: 'image/#'}
     
 // ]
