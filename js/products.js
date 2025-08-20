ab37df07a322f321909f4660a4b98afacc238e4c
FranSanchez <franciscosanchezp44@gmail.com>
2025-08-19 21:17:28 -0300

    carrito con arrays nuevo

---
+++ js/products.js
@@ -0,0 +1,89 @@
+//arrays
+
+const products = [
+    {
+        id:1,
+        nombre: "Red Velvet",
+        imagen: "1.jpg",
+        alt: "Red Velvet",
+        precio: 4900,
+    },
+    {
+        id:2,
+        nombre: "Marquise",
+        imagen: "assets/img/marquise.jpg",
+        alt: "Marquise",
+        precio: 2200,
+    },
+    {
+        id:3,
+        nombre: "Rogel",
+        imagen: "assets/img/rogel.jpg",
+        alt: "Rogel",
+        precio: 2500,
+    },
+    {
+        id:4,
+        nombre: "Matilda",
+        imagen: "assets/img/torta matilda.jpg",
+        alt: "Matilda",
+        precio: 2000,
+    },
+    {
+        id:5,
+        nombre: "Batido de durazno",
+        imagen: "assets/img/batido-Durazno.jpg",
+        alt: "Batido de durazno",
+        precio: 2100,
+    },
+    {
+        id:6,
+        nombre: "Batido de frutilla",
+        imagen: "assets/img/batido-Frutilla.jpg",
+        alt: "Batido de frutilla",
+        precio: 2900,
+    },
+    {
+        id:7,
+        nombre: "batido de kiwi",
+        imagen: "assets/img/batido-Kiwi.jpg",
+        alt: "Batido de kiwi",
+        precio: 3000,
+    },
+    {
+        id:8,
+        nombre:"Batido de sandia",
+        imagen: "assets/img/batido-Sandia.jpg",
+        alt: "Batido de sandia",
+        precio: 2300,
+    },
+    {
+        id:9,
+        nombre: "Helado de Cafe",
+        imagen: "assets/img/helado-Cafe.jpg",
+        alt: "Helado de cafe",
+        precio: 1000,
+
+    },
+    {
+        id:10,
+        nombre: "Helado de chocolate",
+        imagen: "assets/img/helado-Chocolate.jpg",
+        alt: "Helado de Chocolate",
+        precio: 2000,
+    },
+    {
+        id:11,
+        nombre: "Helado de coco",
+        imagen: "assets/img/helado-Coco.jpg",
+        alt: "Helado de coco",
+        precio: 1900,
+    },
+    {
+        id:12,
+        nombre: "Helado de vainilla",
+        imagen: "assets/img/helado-Vainilla.jpg",
+        alt: "Helado de vainilla",
+        precio: 1500,
+    }
+];
---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+const containerCards = document.getElementById("product-container");
+
+
+function createProductStart (productos) {
+    productos.forEach(producto => {
+        const newProduct = document.createElement("div");
+        newProduct.classList = "cardProduct";
+        newProduct.innerHTML = `
+        <h3 class="titleInfo">${producto.nombre}</h3>
+        <img class="imgCardsInfo" src="./assets/img/${producto.id}.jpg">
+        <p>$${producto.precio}</p>
+        <button class="btnSection">Comprar</button>
+        `;
+        containerCards.appendChild(newProduct)
+    });
+}
+
+
+
+createProductStart(products);
