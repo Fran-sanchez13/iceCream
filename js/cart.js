baa6f16f0b65003024707e3595a278e7dc146410
FranSanchez <franciscosanchezp44@gmail.com>
2025-09-02 19:37:18 -0300

    ya se pueden agregar productos al carrito, falta precios y comprar

--- js/cart.js
+++ js/cart.js
@@ -1,20 +1,28 @@
 const containerCards = document.getElementById("product-container");
 
-function createProductStart () {
-    const article = JSON.parse(localStorage.getItem("products"));
-    console.log(article)
-    productos.forEach(producto => {
-        const newProduct = document.createElement("div");
-        newProduct.classList = "cardProduct";
-        newProduct.innerHTML = `
-        <h3 class="titleInfo">${producto.nombre}</h3>
-        <img class="imgCardsInfo" src="./assets/img/${producto.id}.jpg">
-        <p class="price">$${producto.precio}</p>
-        <button class="btnSection">Comprar</button>
-        `;
-        containerCards.appendChild(newProduct);
-        newProduct.querySelector('.btnSection').addEventListener('click', () => addCart(producto))
-    });
+
+function createProductStart() {
+    const productos = JSON.parse(localStorage.getItem("products"));
+    console.log(productos);
+    if (productos && productos.length > 0) {
+
+        productos.forEach(producto => {
+            const newProduct = document.createElement("div");
+            newProduct.classList = "cardProduct";
+            newProduct.innerHTML = `
+            <h3 class="titleInfo">${producto.nombre}</h3>
+            <img class="imgCardsInfo" src="./assets/img/${producto.id}.jpg">
+            <p class="price">$${producto.precio}</p>
+            <div>
+                <button class="btnSection">-</button>
+                <span class="cantidad">0</span>
+                <button class="btnSection">+</button>
+            </div>
+            `;
+            containerCards.appendChild(newProduct);
+            newProduct.querySelector('.btnSection').addEventListener('click', () => addCart(producto))
+        });
+    }
 }
 
 
