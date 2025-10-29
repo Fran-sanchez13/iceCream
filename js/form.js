a44513b013102a6ae408a3d64a30610df1203529
FranSanchez <franciscosanchezp44@gmail.com>
2025-10-28 21:46:25 -0300

    validations finish, sesion no

--- js/form.js
+++ js/form.js
@@ -2,22 +2,19 @@ const formRegister = document.getElementById("formRegister")
 const nameInput = document.getElementById("nombreInput")
 const lastNameInput = document.getElementById("apellidoInput")
 const emailInput = document.getElementById('emailInput')
-const paswordInput = document.getElementById('passwordInput')
+const passwordInput = document.getElementById('passwordInput')
 const phoneInput = document.getElementById('telefonoInput')
 
-const users = [
-    {
-        Nombre: 'nombre',
-        lastName: 'apellido',
-        Email: 'pepito@gmail.com',
-        Password: 'contraseñas',
-        Teléfono: 'telefono'
-    }
-]
+const users = JSON.parse(localStorage.getItem('users')) || []
 
 
 
 /*Funcioones Auxiliares */
+const saveLocalStorage = () => {
+    localStorage.setItem('users', JSON.stringify(users))
+} 
+    
+
 const isEmpty = (input) => {
     return !input.value.trim().length
 }
@@ -33,7 +30,7 @@ const isEmailValid = (input) => {
 }
 
 const isExistingEmail = (input) => {
-   return users.some((user) => user.Email === input.value.trim())
+   return users.some((user) => user.email === input.value.trim())
 }
 
 
@@ -56,9 +53,21 @@ const showSuccess = (input) => {
 
 }
 
+const isPassSecure = (input) => {
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
+
+   return regex.test(input.value.trim())
+}
+
+const isPhoneValid = (input) => {
+    const regex = /^[0-9]{10}$/
+
+    return regex.test(input.value.trim())
+}
+
 /*Funciones de validacion de inputs */
 
-const chackTextInput = (input) => {
+const checkTextInput = (input) => {
     let valid = false
     const maxCharacter = 16
     const minCharacter = 3
@@ -101,6 +110,45 @@ const checkEmail = (input) => {
     return valid
 }
 
+ const chekpassWord = (input) => {
+    let valid = false
+
+    if(isEmpty(input)){
+        showError(input, 'El campo esta vacio')
+        return
+    }
+
+    if(!isPassSecure(input)){
+        showError(input, 'La contraseña debe tener al menos mayuscula, miniscula, un numero y un caracter especial y 8 caracteres ')
+        return
+    }
+    
+
+    showSuccess(input)
+    valid = true
+    return valid
+}
+
+const checkPhone = (input) => {
+    let valid = false
+
+    if(isEmpty(input)){
+        showError(input, 'Este campo es obligatorio')
+        return
+    }
+
+    if(!isPhoneValid(input)){
+        showError(input, 'El campo solo debe tener 10 caracteres numericos')
+        return
+    }
+    //que tenga 10 caracteres
+
+
+    showSuccess(input)
+    valid = true
+    return valid
+
+}
 
 
 /*Validacion general y almacenamiento de datos */
@@ -108,18 +156,48 @@ const submitHandler = (e) => {
     //prevenimos comportamiento por defecto
     e.preventDefault()
     //validar los inputs
-    //agregar al usuario
-    //guardar en el localStorage
-    //informar al usuario(con un alert)
-    //redirigir al login
+    let isNameValid = checkTextInput(nameInput)
+    let islastNameValid = checkTextInput(lastNameInput)
+    let isEmailValid = checkEmail(emailInput)
+    let isPassValid = chekpassWord(passwordInput)
+    let isPhoneValid = checkPhone(phoneInput)
+
+    let isValidForm =
+        isNameValid &&
+        islastNameValid &&
+        isEmailValid &&
+        isPassValid &&
+        isPhoneValid
+
+    if(isValidForm){
+        //agregar al usuario
+        users.push({
+            name: nameInput.value.trim(),
+            lastName: lastNameInput.value.trim(),
+            password: passwordInput.value.trim(),
+            email: emailInput.value.trim(),
+            phone: phoneInput.value.trim()
+        })
+        //guardar en el localStorage
+        saveLocalStorage()
+        //informar al usuario(con un alert)
+        alert('Te registraste con exito!!')
+        //redirigir al login
+        window.location.href = 'sesion.html'
+
+    }
+        
+
 }
 
 
 const init = () => {
     formRegister.addEventListener('submit', submitHandler)
-    nameInput.addEventListener('input', () => chackTextInput(nameInput))
-    lastNameInput.addEventListener('input', () => chackTextInput(lastNameInput))
+    nameInput.addEventListener('input', () => checkTextInput(nameInput))
+    lastNameInput.addEventListener('input', () => checkTextInput(lastNameInput))
     emailInput.addEventListener('input', () => checkEmail(emailInput))
+    passwordInput.addEventListener('input', () => chekpassWord(passwordInput))
+    phoneInput.addEventListener('input', () => checkPhone(phoneInput))
 }
 
 init()
